58d5eac46f5a1bb1061347a1746e62c959675c96
Dheeresh Kumar <141610112+Tech-Dheer@users.noreply.github.com>
2024-03-15 16:31:30 +0530

    Add files via upload

---
+++ Day 3/EmployeeData.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import './EmployeeData.css';
+
+function EmployeeData() {
+    const [deptsArray, setDeptsArray] = useState([]);
+    const [deptno, setDeptno] = useState("");
+    const [dname, setDname] = useState("");
+    const [loc, setLoc] = useState("");
+
+    function getDeptsButton_click() {
+        let tempArray = [
+            { deptno: 10, dname: "Accounts", loc: "Hyd" },
+            { deptno: 20, dname: "Sales", loc: "Pune" },
+            { deptno: 30, dname: "Marketing", loc: "Hyd" },
+            { deptno: 40, dname: "Operations", loc: "Chennai" },
+        ];
+        setDeptsArray(tempArray);
+    }
+
+    function addDeptButton_click() {
+        let deptObj = { deptno, dname, loc };
+        let tempArray = [...deptsArray, deptObj];
+        setDeptsArray(tempArray);
+        clearFields();
+    }
+
+    function clearFields() {
+        setDeptno("");
+        setDname("");
+        setLoc("");
+    }
+
+    function deleteDept_click(dno) {
+        if (window.confirm('Are you sure you want to delete this?')) {
+            let tempArray = deptsArray.filter(item => item.deptno !== dno);
+            setDeptsArray(tempArray);
+        }
+    }
+
+    function selectDept_click(dno) {
+        let deptObj = deptsArray.find(item => item.deptno === dno);
+        setDeptno(deptObj.deptno);
+        setDname(deptObj.dname);
+        setLoc(deptObj.loc);
+    }
+
+    function updateDeptButton_click() {
+        let tempArray = [...deptsArray];
+        let index = tempArray.findIndex(item => item.deptno === deptno);
+        tempArray[index].dname = dname;
+        tempArray[index].loc = loc;
+        setDeptsArray(tempArray);
+        clearFields();
+    }
+
+    let resultArray = deptsArray.map((item, index) => (
+        <tr key={index}>
+            <td>{item.deptno}</td>
+            <td>{item.dname}</td>
+            <td>{item.loc}</td>
+            <td className="action-icons">
+                <img src='./images/edit.png' alt="Edit" onClick={() => selectDept_click(item.deptno)} />
+                <img src='./images/delete.png' alt="Delete" onClick={() => deleteDept_click(item.deptno)} />
+            </td>
+        </tr>
+    ));
+
+    return (
+        <>
+            <h3>Working with State -- CRUD Operations on Array of Objects</h3>
+            <hr />
+            <input type="text" placeholder="Dept Number" value={deptno} onChange={(e) => setDeptno(e.target.value)} />
+            <input type="text" placeholder="Dept Name" value={dname} onChange={(e) => setDname(e.target.value)} />
+            <input type="text" placeholder="Dept Location" value={loc} onChange={(e) => setLoc(e.target.value)} />
+            <hr />
+            <input type="button" onClick={getDeptsButton_click} value="Get Depts" />
+            <input type="button" onClick={addDeptButton_click} value="Add Dept" />
+            <input type="button" onClick={updateDeptButton_click} value="Update Dept" />
+            <hr />
+            <table border="2" width="500" cellSpacing="0" cellPadding="5">
+                <thead>
+                    <tr>
+                        <th>Dept Number</th>
+                        <th>Dept Name</th>
+                        <th>Location</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {resultArray}
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+export default EmployeeData;
